35b15e873a9acb955f26d25c92303b60f0416048
AnthonyLoche <anthonylocheifc@gmail.com>
2024-03-31 17:38:49 -0300

    Model Compra adicionada

--- livraria/admin.py
+++ livraria/admin.py
@@ -1,11 +1,14 @@
 from django.contrib import admin
-from livraria.models import Autor, Categoria, Editora, Livro
+from livraria.models import Autor, Categoria, Editora, Livro, Compra, ItensCompra
 # Register your models here.
 
 # admin.site.register(Categoria)
 # admin.site.register(Editora)
 # admin.site.register(Livro)
 # admin.site.register(Autor)
+
+admin.site.register(Compra)
+admin.site.register(ItensCompra)
 
 @admin.register(Autor)
 class AutorAdmin(admin.ModelAdmin):
