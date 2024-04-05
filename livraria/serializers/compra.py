7567d7f1a276dc3a191c1d6946678463c0cd7380
AnthonyLoche <anthonylocheifc@gmail.com>
2024-04-05 15:12:14 -0300

    Feito até parte 31

--- livraria/serializers/compra.py
+++ livraria/serializers/compra.py
@@ -1,8 +1,50 @@
-from rest_framework.serializers import ModelSerializer, CharField
+from rest_framework.serializers import ModelSerializer, CharField, SerializerMethodField
 
-from livraria.models import Compra
+from livraria.models import Compra, ItensCompra
+
+class ItensCompraSerializer(ModelSerializer):
+    total = SerializerMethodField()
+    class Meta:
+        model = ItensCompra
+        fields = ["livro", "quantidade", "total"]
+        depth = 2
+        
+    def get_total(self, instance):
+        return instance.quantidade * instance.livro.preco
 
 class CompraSerializer(ModelSerializer):
+    usuario_email = CharField(source="usuario.email", read_only=True)
+    status = CharField(source="get_status_display", read_only=True)
+    itens = ItensCompraSerializer(many=True, read_only=True)
     class Meta:
         model = Compra
-        fields = "__all__"
+        fields = ("id", "usuario_email", "status", "total", "itens")
+
+    def update(self, instance, validated_data):
+        itens = validated_data.pop("itens")
+        if itens:
+            instance.itens.all().delete()
+            for item in itens:
+                ItensCompra.objects.create(compra=instance, **item)
+        instance.save()
+        return instance
+
+class CriarEditarItensCompraSerializer(ModelSerializer):
+    class Meta:
+        model = ItensCompra
+        fields = ("livro", "quantidade")
+
+class CriarEditarCompraSerializer(ModelSerializer):
+    itens = CriarEditarItensCompraSerializer(many=True) # Aqui mudou
+
+    class Meta:
+        model = Compra
+        fields = ("usuario", "itens")
+
+    def create(self, validated_data):
+        itens_data = validated_data.pop("itens")
+        compra = Compra.objects.create(**validated_data)
+        for item_data in itens_data:
+            ItensCompra.objects.create(compra=compra, **item_data)
+        compra.save()
+        return compra
