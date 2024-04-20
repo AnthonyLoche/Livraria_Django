3e17ff5afec8895655f98c547432fddba214c09d
AnthonyLoche <anthonylocheifc@gmail.com>
2024-04-20 14:35:55 -0300

    Feito até a parte 38

--- livraria/views/livro.py
+++ livraria/views/livro.py
@@ -1,6 +1,7 @@
 from rest_framework.viewsets import ModelViewSet
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import viewsets
+from rest_framework.filters import SearchFilter, OrderingFilter
 
 from livraria.models import Livro
 
@@ -13,8 +14,11 @@ from livraria.serializers import (
 class LivroViewSet(viewsets.ModelViewSet):
     queryset = Livro.objects.all()
     serializer_class = LivroSerializer
-    filter_backends = [DjangoFilterBackend]
+    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
     filterset_fields = ["categoria__descricao", "editora__nome", "autor__nome", "compra__status"]
+    search_fields = ["titulo", "autor", "editora", "categoria"]
+    ordering_fields = ["titulo", "preco"]
+    ordering = ["titulo"]
 
 
 def get_serializer_class(self):
